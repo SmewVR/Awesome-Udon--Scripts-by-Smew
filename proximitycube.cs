8b97abe2ae7221f8d53c6c9570cb880fdb3b85b6
SmewVR <93958928+SmewVR@users.noreply.github.com>
2022-07-23 11:58:27 -0500

    Add files via upload

---
+++ proximitycube.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class proximitycube : UdonSharpBehaviour
+{
+    public GameObject[] target_list;
+    
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player == Networking.LocalPlayer)
+        {
+
+            for (int i = 0; i < target_list.Length; i++)
+            {
+                target_list[i].SetActive(true);
+            }
+        }
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (player == Networking.LocalPlayer)
+        {
+            for (int i = 0; i < target_list.Length; i++)
+            {
+                target_list[i].SetActive(false);
+            }
+        }
+    }
+}
